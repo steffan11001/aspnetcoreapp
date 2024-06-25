b636e26c4fbd78b9643d4a5cf9b250c8162c3394
Cosma Stefan <cosma.steffan11@gmail.com>
2024-06-25 19:35:55 +0300

    Add gitignore

--- obj/Debug/net8.0/aspnetcoreapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/aspnetcoreapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f6ef29014a32f1f930c2cdf19d152d0ac58e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
